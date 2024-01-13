320fe2c5681de81d7c5e6c4d66f0fcbba80c2b20
coldpon <95997133+coldpon@users.noreply.github.com>
2024-01-14 03:50:37 +0800

    Update mainWindow.h

--- System提权工具/mainWindow.h
+++ System提权工具/mainWindow.h
@@ -3,6 +3,8 @@
 #include<TlHelp32.h>
 #include<filesystem>
 #include<iostream>
+#include <shellscalingapi.h>
+#pragma comment(lib,"Shcore.lib")
 #ifdef _UNICODE
 #if defined _M_IX86
 #pragma comment(linker,"/manifestdependency:\"type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='x86' publicKeyToken='6595b64144ccf1df' language='*'\"")
